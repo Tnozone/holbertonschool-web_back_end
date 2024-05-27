da2f463fbff3dcac8b3aef855d3cbb0ff2a58897
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-05-27 10:16:50 +0200

    fixed def put

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
 """ Basic Cache
 """
 
 from base_caching import BaseCaching
+
 
 class BasicCache(BaseCaching):
     """Basic dictionary"""
 
     def put(self, key, item):
         """Puts item in cache"""
-        if item is not None and item is not None:
+        if item is not None and key is not None:
             self.cache_data[key] = {item}
 
     def get(self, key):
