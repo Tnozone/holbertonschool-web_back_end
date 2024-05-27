774e72a3ad9432c4edf8e5c3e84fe2a1cbb4d74a
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-05-27 09:58:09 +0200

    caching dir and task 0

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Basic Cache
+"""
+
+from base_caching import BaseCaching
+
+class BasicCache(BaseCaching):
+    """Basic dictionary"""
+
+    def put(self, key, item):
+        """Puts item in cache"""
+        if item is not None and item is not None:
+            self.cache_data[key] = {item}
+
+    def get(self, key):
+        """Retreave item from cache"""
+        return self.cache_data.get(key, None)
