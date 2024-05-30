ae993219a567f8ffe0bb47a741de33f78959c056
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-05-30 11:59:52 +0200

    fix dictionary

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,19 +1,20 @@
-#!/usr/bin/python3
-
-""" Basic Cache
-"""
+#!/usr/bin/env python3
+""" Basic Cache module """
 
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Basic dictionary"""
+    """bsic dictionary"""
 
     def put(self, key, item):
-        """Puts item in cache"""
-        if item is not None and key is not None:
-            self.cache_data[key] = {item}
+        """ Puts item in cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Retreave item from cache"""
+        """ Gets item from cache """
+        if key is None:
+            return None
         return self.cache_data.get(key, None)
