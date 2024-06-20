2677609468b4a1f933b86519fd72ded30bdae338
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-06-20 19:06:35 +0200

    pystyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -37,14 +37,12 @@ class Auth:
 
         return True
 
-
     def authorization_header(self, request=None) -> str:
         """ Method that handles authorization header """
         if request is None:
             return None
 
         return request.headers.get("Authorization", None)
-
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Validates current user """
