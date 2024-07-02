8991dd25b29ab7bae8a47986f13013a57b8527c4
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-07-02 13:08:10 +0200

    unittests dir

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Test utils functions.
+"""
+
+import utils
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Class to test nested maps
+    """
+
+    @TestAccessNestedMap.test_access_nested_map
