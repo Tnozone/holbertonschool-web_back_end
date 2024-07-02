fbc10945bf5f9d5b89849655567ea9b5067ec49d
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-07-02 19:29:00 +0200

    task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,7 @@
 """Test utils functions.
 """
 
-import utils
+from utils import access_nested_map
 import unittest
 
 
@@ -10,4 +10,21 @@ class TestAccessNestedMap(unittest.TestCase):
     """Class to test nested maps
     """
 
-    @TestAccessNestedMap.test_access_nested_map
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """ Test that the method returns what it is supposed to """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for the respective inputs """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
