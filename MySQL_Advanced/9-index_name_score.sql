8917f992ce73c3ec0e48c00255a47d4f53415491
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-08-08 19:37:24 +0200

    correct index name

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- 9. Optimize search and score
--- creates an index
-CREATE INDEX idx_name_first
+-- creates an index on the table names and the first letter of name and the score
+CREATE INDEX idx_name_first_score
 ON names (name(1), score);
