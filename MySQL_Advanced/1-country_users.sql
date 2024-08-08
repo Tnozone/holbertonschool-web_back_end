26e628b167d3ab20023e6fe16df741fa2bf509e6
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-08-08 18:07:03 +0200

    what not working

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT, 
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN'), NOT NULL,
+    country ENUM('US', 'CO', 'TN') NOT NULL,
     PRIMARY KEY (id)
 );
