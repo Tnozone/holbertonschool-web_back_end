a54c27266f9b1e41d3abc39fe595f4a8d37e61ed
Maxime <maxime@MacBook-Pro-de-Jacques-2.local>
2024-08-08 19:04:27 +0200

    task 5

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- 5. Email validation to sent
+-- creates a trigger that resets email
+DELIMITER $$ 
+CREATE TRIGGER email_trigger
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+IF NEW.email <> OLD.email
+THEN
+    SET NEW.valid_email = 0;
+END IF;
+END
+$$
+DELIMITER ;
